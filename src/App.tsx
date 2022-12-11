14e6b7b6bc83e8df4a79c94037ba19e70884bf88
MD Nelles <devstudioca@gmail.com>
2022-12-11 05:40:40 -0500

    refactore face types

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
 //import { Cuboid,Card } from "anim-3d-obj";
 
 import Obj from "./components/Obj";
+import { AnimSpecsType } from "./components/styles/Anim";
+import { FaceType, GlobalType } from "./components/types";
 
 function App() {
-   const faceprops = {
-      front: true,
-      back: false,
-      left: false,
-      right: false,
-      top: false,
-      top_rear: false,
-      top_front: false,
-      bottom_rear: false,
-      bottom_front: false,
-      bottom: false,
+   // face individual styles (over rides global)
+   const exampleCss = `border: 2px solid #900;
+   color:#900;
+   backface-visibility: visible;
+   background:rgba(220,22,22,.3);
+   text-align:center;
+   line-height:5;
+   font-family: Arial;`;
+   const global: GlobalType = {
+      body: "GLOBAL",
    };
-
-   const global: object = {
-      // // face individual styles (over rides global)
-      css: `
-         border: 1px solid #333;
-         color:#333;
-         backface-visibility: visible;
-         text-align:center;
-         line-height:10;
-         font-family: Arial, Helvetica, sans-serif;
-         `,
-      body: " ",
-   };
-   const anim1: object = {
+   const anim1: AnimSpecsType = {
       border: "",
       degreesHi: -15, // degrees if spin
       degreesLow: 15, // degrees if spin
@@ -36,56 +24,71 @@ function App() {
       direction: "normal", //normal altenating reverse
       duration: 5,
       fillMode: "forward", // node forward backward both
-      iterationCount: 1,
-      name: "swingDecay",
+      iterationCount: "infinite",
+      name: "X360",
       timing: "ease-in-out", // linear ease ease-in-out
    };
-   const anim2: object = {
+   const anim2: AnimSpecsType = {
       border: "",
       degreesHi: 15,
       degreesLow: -15,
       delay: 0,
       direction: "normal",
-      duration: 38,
+      duration: 18,
       fillMode: "forward",
       iterationCount: "infinite",
-      name: "",
+      name: "Y360",
       timing: "ease-in-out",
    };
 
-   const details = `backface-visibility: visible;
-   font-size:20px;
-   text-align:center;
-   font-family: Arial, Helvetica, sans-serif;
-   line-height:9;`;
-
-   const custom: object = {
-      // face individual styles (over rides global)
-
-      front: {
-         css: ``,
-         body: "FRONT",
+   // face individual styles (over rides global)
+   const faces: FaceType[] = [
+      {
+         name: "back",
+         body: "BACK",
+         css: exampleCss,
       },
+      {
+         name: "right",
+         body: "RIGHT",
+         css: exampleCss,
+      },
+      {
+         name: "top",
+         body: "TOP",
+         css: exampleCss,
+      },
+      {
+         name: "front",
+         body: "FRONT",
+         css: exampleCss,
+      },
+      {
+         name: "left",
+         body: "LEFT",
+         css: exampleCss,
+      },
+      {
+         name: "bottom",
+         body: "BOTTOM",
+         css: exampleCss,
+      },
+   ];
+   const objProps = {
+      width: 150,
+      height: 150,
+      depth: 150,
+      perspectiveOrigin: "50% 50%",
+      perspective: 900,
+      faces,
+      anim1,
+      anim2,
+      global,
+      showCenterDiv: false,
    };
-
    return (
       <div style={{ padding: 300 }}>
-         <Obj
-            width={150}
-            height={150}
-            depth={1}
-            perspectiveOrigin='50% 50%'
-            perspective={900}
-            zIndex={10}
-            custom={custom}
-            anim1={anim1}
-            anim2={anim2}
-            faces={faceprops}
-            global={global}
-            showCenterDiv={false}
-         >
-            {}
-         </Obj>
+         <Obj {...objProps} />
          <div style={{ padding: 5 }} />
       </div>
    );
